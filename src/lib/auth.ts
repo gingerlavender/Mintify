ed7599caae734e4441195ea7cac3eb345ea72deb
gingerlavender <vkurnosoff76@gmail.com>
2025-06-16 04:08:20 +0300

    spotify

---
+++ src/lib/auth.ts
@@ -0,0 +1,26 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { AuthOptions } from "next-auth";
+import SpotifyProvider from "next-auth/providers/spotify";
+import { prisma } from "./prisma";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_CLIENT_ID!,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
+      authorization: {
+        params: { scope: "user-read-email user-read-private user-top-read" },
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
