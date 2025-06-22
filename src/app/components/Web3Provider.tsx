e125f86ee49da2c967fda0ee6b7d1575cd62edc7
gingerlavender <vkurnosoff76@gmail.com>
2025-06-22 22:48:59 +0300

    optimism sepolia

--- src/app/components/Web3Provider.tsx
+++ src/app/components/Web3Provider.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Config, WagmiProvider, http } from "wagmi";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { mainnet, optimism } from "wagmi/chains";
+import { mainnet, optimism, optimismSepolia } from "wagmi/chains";
 import { useEffect, useState } from "react";
 
 export function Web3Provider({ children }: { children: React.ReactNode }) {
@@ -16,10 +16,19 @@ export function Web3Provider({ children }: { children: React.ReactNode }) {
     const cfg = getDefaultConfig({
       appName: "Mintify",
       projectId: `${process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID}`,
-      chains: [mainnet, optimism],
+      chains: [
+        mainnet,
+        optimism,
+        ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS == "true"
+          ? [optimismSepolia]
+          : []),
+      ],
       transports: {
         [mainnet.id]: http(`${process.env.NEXT_PUBLIC_ALCHEMY_MAINNET_URL}`),
         [optimism.id]: http(`${process.env.NEXT_PUBLIC_ALCHEMY_OPMAINNET_URL}`),
+        [optimismSepolia.id]: http(
+          `${process.env.NEXT_PUBLIC_ALCHEMY_OPSEPOLIA_URL}`
+        ),
       },
       ssr: true,
     });
