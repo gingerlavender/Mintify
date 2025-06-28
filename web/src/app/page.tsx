60e7726e638817e8a357c25c1808dff8380aeef8
gingerlavender <vkurnosoff76@gmail.com>
2025-06-28 06:35:19 +0300

    replaced WalletGuard inside ModalProvider

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -13,10 +13,10 @@ export default function Home() {
   return (
     <SessionProvider>
       <Web3Provider>
-        <WalletGuard />
         <Soundwave />
         <Header />
         <ModalProvider>
+          <WalletGuard />
           <Menu />
         </ModalProvider>
       </Web3Provider>
