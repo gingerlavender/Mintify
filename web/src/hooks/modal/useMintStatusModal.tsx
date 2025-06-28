18604b7d764a5b1ff66a95afecc837038a67bf5e
gingerlavender <vkurnosoff76@gmail.com>
2025-06-28 03:37:23 +0300

    useEffect() in modal content

--- web/src/hooks/modal/useMintStatusModal.tsx
+++ web/src/hooks/modal/useMintStatusModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 import { MintStatus } from "@/types/mint";
 import { getMintStatus } from "@/lib/mint";
@@ -13,76 +13,65 @@ const messages: Record<MintStatus, string> = {
     "You can see yout current NFT below. Remember you can remint it any time!",
 };
 
-const MintStatusModalContent = ({
-  message,
-  picture,
-  closeModal,
-}: {
-  message: string;
-  picture: string;
-  closeModal: () => void;
-}) => {
-  return (
-    <>
-      <p>{message}</p>
-      <img
-        className="w-[50vw] md:w-[20vw] rounded-2xl"
-        src={picture}
-        alt="NFT Preview"
-      />
-      <div className="flex justify-center gap-4">
-        <button
-          className="bg-white/100 rounded-xl p-3 md:min-w-[10vw] min-w-[30vw] font-[400] hover:scale-[103%] hover:bg-white/60 cursor-pointer transition-all duration-200 shadow-xs"
-          onClick={closeModal}
-        >
-          Mint
-        </button>
-      </div>
-    </>
-  );
-};
-
 export const useMintStatusModal = () => {
-  const [message, setMessage] = useState<string>("");
-  const [picture, setPicture] = useState<string>("/NFTPlaceholder.png");
-
   const { address } = useAccount();
 
   const { openModal, closeModal, endLoading } = useModal();
+
+  const MintStatusModalContent = () => {
+    const [message, setMessage] = useState<string>("");
+    const [picture, setPicture] = useState<string>("/NFTPlaceholder.png");
+
+    useEffect(() => {
+      (async () => {
+        try {
+          if (!address) {
+            throw new Error("Missing wallet address");
+          }
+          const data = await getMintStatus(address);
+          setMessage(messages[data.mintStatus]);
+          if (data.mintStatus == "repeated") {
+            setPicture(data.tokenURI);
+          }
+        } catch (error) {
+          if (error instanceof Error) {
+            setMessage(`Error: ${error.message}`);
+          } else {
+            setMessage("Unknown error");
+          }
+          setPicture("/Error.png");
+        } finally {
+          endLoading();
+        }
+      })();
+    }, []);
+
+    return (
+      <>
+        <p>{message}</p>
+        <img
+          className="w-[50vw] md:w-[20vw] rounded-2xl"
+          src={picture}
+          alt="NFT Preview"
+        />
+        <div className="flex justify-center gap-4">
+          <button
+            className="bg-white/100 rounded-xl p-3 md:min-w-[10vw] min-w-[30vw] font-[400] hover:scale-[103%] hover:bg-white/60 cursor-pointer transition-all duration-200 shadow-xs"
+            onClick={closeModal}
+          >
+            Mint
+          </button>
+        </div>
+      </>
+    );
+  };
 
   const openMintStatusModal = async () => {
     openModal({
       title: "Your Mint Status",
       loading: true,
-      content: (
-        <MintStatusModalContent
-          message={message}
-          picture={picture}
-          closeModal={closeModal}
-        />
-      ),
+      content: <MintStatusModalContent />,
     });
-
-    try {
-      if (!address) {
-        throw new Error("Missing wallet address");
-      }
-
-      const data = await getMintStatus(address);
-      setMessage(messages[data.mintStatus]);
-      if (data.mintStatus == "repeated") {
-        setPicture(data.tokenURI);
-      }
-    } catch (error) {
-      if (error instanceof Error) {
-        setMessage(`Error: ${error.message}`);
-      } else {
-        setMessage("Unknown error");
-      }
-      setPicture("/Error.png");
-    } finally {
-      endLoading();
-    }
   };
 
   return { openMintStatusModal };
