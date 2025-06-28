aefedfa3f091077113a6f58a57d5aa0a73263182
gingerlavender <vkurnosoff76@gmail.com>
2025-06-28 22:00:53 +0300

    loading on spotify auth

--- web/src/hooks/useLoading.tsx
+++ web/src/hooks/useLoading.tsx
@@ -3,8 +3,8 @@ import { useCallback, useState } from "react";
 export const useLoading = (initial: boolean) => {
   const [isLoading, setIsLoading] = useState<boolean>(initial);
 
-  const startLoading = useCallback(() => setIsLoading(false), []);
-  const endLoading = useCallback(() => setIsLoading(true), []);
+  const startLoading = useCallback(() => setIsLoading(true), []);
+  const endLoading = useCallback(() => setIsLoading(false), []);
 
   return { isLoading, startLoading, endLoading };
 };
