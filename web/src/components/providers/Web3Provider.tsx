1770c127ea8f4e06d32153157dc236179ba9a59a
gingerlavender <vkurnosoff76@gmail.com>
2025-07-10 01:39:28 +0300

    removed mainnet

--- web/src/components/providers/Web3Provider.tsx
+++ web/src/components/providers/Web3Provider.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Config, WagmiProvider, http } from "wagmi";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { mainnet, optimism, optimismSepolia } from "wagmi/chains";
+import { optimism, optimismSepolia } from "wagmi/chains";
 
 export function Web3Provider({ children }: { children: React.ReactNode }) {
   const [config, setConfig] = useState<Config>();
@@ -16,14 +16,12 @@ export function Web3Provider({ children }: { children: React.ReactNode }) {
       appName: "Mintify",
       projectId: `${process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID}`,
       chains: [
-        mainnet,
         optimism,
         ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS == "true"
           ? [optimismSepolia]
           : []),
       ],
       transports: {
-        [mainnet.id]: http(`${process.env.NEXT_PUBLIC_ALCHEMY_MAINNET_URL}`),
         [optimism.id]: http(`${process.env.NEXT_PUBLIC_ALCHEMY_OPMAINNET_URL}`),
         [optimismSepolia.id]: http(
           `${process.env.NEXT_PUBLIC_ALCHEMY_OPSEPOLIA_URL}`
